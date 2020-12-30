ed1d5e25f79cbe4cc67272c4e11040fafde52907
Vakil <vakilverma7@yahoo.com>
2020-12-30 14:20:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react'
 function App() {
+  const [item, setitem] = useState();
+  const [names, setname] = useState([]);
+  const changeMe=(e)=>{
+    setitem(e.target.value);
+  }
+  const Handle=()=>{
+   setname((old)=>{
+     return [...old,item];
+   });
+   setitem('');
+  }
+  const delteItem=(id)=>{
+    setname((old)=>{
+     return old.filter((a,index)=>{
+       return index!==id;
+     })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main">
+        <h1>Todo List app</h1>
+        <input type="text"
+        onChange={changeMe}
+        value={item}
+         placeholder="Enter item"/> 
+        <button onClick={Handle}>+</button>
+        <ol>
+          {names.map((res,index)=>{
+            return <li><span onClick={()=>delteItem(index)}
+            style={{fontSize:'32px'}}>*</span> {res}</li>
+  
+          })}
+        </ol>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
